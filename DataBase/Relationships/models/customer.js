9aa3ac9cfe640b6f9ab922c29967dfa0ad08a6de
Avneesh Sharma <161005348+AvneeshS11@users.noreply.github.com>
2026-03-21 11:47:54 +0530

    updated

--- DataBase/Relationships/models/customer.js
+++ DataBase/Relationships/models/customer.js
@@ -1,100 +1,87 @@
 const mongoose = require('mongoose');
 
+// -------------------- DB CONNECTION --------------------
 main()
-    .then(() => console.log('Connection Successful')) // agar connect ho gaya
-    .catch(err => console.log(err)); // agar error aaya
+    .then(() => console.log('Connection Successful')) 
+    .catch(err => console.log(err)); 
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
 }
 
 // -------------------- ORDER SCHEMA --------------------
-
-// (structure of Order document)
+// Order = ek alag collection (table jaisa)
 const orderSchema = new mongoose.Schema({
-    item: String,  
-    price: Number  
+    item: String,
+    price: Number
 })
 
 // -------------------- CUSTOMER SCHEMA --------------------
-
-// Customer ka schema define kar rahe hain
+// Customer ke andar orders ka reference store ho raha hai (relation)
 const customerSchema = new mongoose.Schema({
-    name : String, 
-
-    // orders ek array hai jisme multiple orders store honge
-    orders :[
+    name: String,
+    orders: [
         {
-            type : mongoose.Schema.Types.ObjectId, // yeh ID store karega
-            ref : 'Order' // yeh batata hai ki yeh ID Order collection se linked hai
+            type: mongoose.Schema.Types.ObjectId, // foreign key (Order ka _id)
+            ref: 'Order' // populate ke liye batata hai kis collection se data aayega
         }
     ]
 })
 
+// -------------------- MIDDLEWARE --------------------
+// Jab customer delete hoga, uske related orders bhi delete ho jayenge
+customerSchema.post('findOneAndDelete', async (cust) => {
+    console.log('Post Middleware Triggered');
+
+    if (cust.orders.length) {
+        let res = await Order.deleteMany({ _id: { $in: cust.orders } });
+        console.log(res);
+    }
+})
+
 // -------------------- MODELS --------------------
-
-// schema ko model me convert kar rahe hain (collection create hota hai)
+// Models = collections (MongoDB me tables jaisa)
 const Order = mongoose.model('Order', orderSchema);
-const Customer = mongoose.model('Customer',customerSchema);
+const Customer = mongoose.model('Customer', customerSchema);
 
-// -------------------- ADD CUSTOMER FUNCTION --------------------
+// -------------------- ADD CUSTOMER --------------------
+const addCustomer = async () => {
+    let cust1 = new Customer({ name: 'Avneesh' });
 
-// naya customer add karne ka function
-const addCustomer = async ()=>{
+    let order1 = await Order.findOne({ item: 'Samosa' });
+    let order2 = await Order.findOne({ item: 'Chips' });
 
-    // new customer object create kiya
-    let cust1 = new Customer({
-        name : 'Avneesh',
-    })
-
-    // DB se ek order fetch kar rahe hain (Samosa)
-    let order1 = await Order.findOne({item : 'Samosa'});
-
-    // DB se ek aur order fetch kar rahe hain (Chips)
-    let order2 = await Order.findOne({item : 'Chips'});
-
-    // orders array me order1 ki ObjectId push kar rahe hain
-    cust1.orders.push(order1);
-
-    // orders array me order2 ki ObjectId push kar rahe hain
+    cust1.orders.push(order1); // ObjectId store hoga
     cust1.orders.push(order2);
-    
-    // customer ko DB me save kar rahe hain
-    let res = await cust1.save();
 
-    // result print kar rahe hain
+    let res = await cust1.save();
     console.log(res);
 }
- // isko run karoge to customer insert hoga
-// addCustomer() 
+// addCustomer();
 
 // -------------------- FIND + POPULATE --------------------
-
-// data fetch karne ka function
-const find = async ()=>{
-
-    // sabhi customers ko fetch kar rahe hain
-    // populate('orders') ka matlab:
-    // orders me jo ObjectId hai uski jagah full Order document laao
+// populate = ObjectId ko actual document me convert karta hai
+const find = async () => {
     let res = await Customer.find({}).populate('orders');
-
-    // first customer print kar rahe hain
     console.log(res[0]);
+}
+// find();
 
+// -------------------- ADD ONE CUSTOMER --------------------
+const addOne = async () => {
+    let newcust = new Customer({ name: 'Arpit' });
+    let order1 = await Order.findOne({ item: 'pizza' });
+
+    newcust.orders.push(order1);
+    await newcust.save();
+}
+// addOne();
+
+// -------------------- DELETE CUSTOMER --------------------
+// delete ke baad middleware chalega (cascade delete jaisa behavior)
+const remove = async () => {
+    let del = await Customer.findByIdAndDelete('69be2a7ee2350df533fb7ce3');
+    console.log(del);
 }
 
-// function call
-find();
-
-// -------------------- ADD ORDERS FUNCTION --------------------
-
-// multiple orders ek saath insert karne ka function
-// const addOrder = async () => {
-//     let res = await Order.insertMany([
-//         { item: 'Samosa', price: 50 },
-//         { item: 'Chips', price: 20 },
-//         { item: 'momo', price: 30 },
-//     ])
-//     console.log(res)
-// }
-// addOrder()
+// remove();
